8e5f8e478ede95f4d5fdc65bb027450b04ebcc9d
Jlumin <45889274+Jlumin@users.noreply.github.com>
2020-07-09 13:33:18 +0800

    Add files via upload

--- up_file.py
+++ up_file.py
@@ -31,7 +31,7 @@ def update_file(service, update_drive_service_name, local_file_path, update_driv
     :param local_file_path: 本地端的位置
     :param local_file_name: 本地端的檔案名稱
     """
-    print("正在上傳檔案...")
+    #print("正在上傳檔案...")
     if update_drive_service_folder_id is None:
         file_metadata = {'name': update_drive_service_name}
     else:
@@ -44,11 +44,11 @@ def update_file(service, update_drive_service_name, local_file_path, update_driv
     start = time.time()
     file_id = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     end = time.time()
-    print("上傳檔案成功！")
-    print('雲端檔案名稱為: ' + str(file_metadata['name']))
-    print('雲端檔案ID為: ' + str(file_id['id']))
-    print('檔案大小為: ' + str(file_metadata_size) + ' byte')
-    print("上傳時間為: " + str(end-start))
+    #print("上傳檔案成功！")
+    #print('雲端檔案名稱為: ' + str(file_metadata['name']))
+    #print('雲端檔案ID為: ' + str(file_id['id']))
+    #print('檔案大小為: ' + str(file_metadata_size) + ' byte')
+    #print("上傳時間為: " + str(end-start))
 
     return file_metadata['name'], file_id['id']
 
@@ -61,16 +61,16 @@ def search_folder(service, update_drive_folder_name=None):
     :return:
     """
     get_folder_id_list = []
-    print(len(get_folder_id_list))
+    #print(len(get_folder_id_list))
     if update_drive_folder_name is not None:
         response = service.files().list(fields="nextPageToken, files(id, name)", spaces='drive',
                                        q = "name = '" + update_drive_folder_name + "' and mimeType = 'application/vnd.google-apps.folder' and trashed = false").execute()
         for file in response.get('files', []):
             # Process change
-            print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
+            #print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
             get_folder_id_list.append(file.get('id'))
         if len(get_folder_id_list) == 0:
-            print("你給的資料夾名稱沒有在你的雲端上！，因此檔案會上傳至雲端根目錄")
+            #print("你給的資料夾名稱沒有在你的雲端上！，因此檔案會上傳至雲端根目錄")
             return None
         else:
             return get_folder_id_list
@@ -94,12 +94,12 @@ def search_file(service, update_drive_service_name, is_delete_search_file=False)
     if not items:
         print('沒有發現你要找尋的 ' + update_drive_service_name + ' 檔案.')
     else:
-        print('搜尋的檔案: ')
+        #print('搜尋的檔案: ')
         for item in items:
             times = 1
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+            #print(u'{0} ({1})'.format(item['name'], item['id']))
             if is_delete_search_file is True:
-                print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
+                #print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
                 delete_drive_service_file(service, file_id=item['id'])
 
             if times == len(items):
@@ -121,11 +121,11 @@ def trashed_file(service, is_delete_trashed_file=False):
     if not items:
         print('垃圾桶無任何資料.')
     else:
-        print('垃圾桶檔案: ')
+        #print('垃圾桶檔案: ')
         for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+            #print(u'{0} ({1})'.format(item['name'], item['id']))
             if is_delete_trashed_file is True:
-                print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
+                #print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
                 delete_drive_service_file(service, file_id=item['id'])
 
 
@@ -137,8 +137,10 @@ def main(is_update_file_function=False, update_drive_service_folder_name=None, u
     :param update_file_path: 要上傳檔案的位置以及名稱
     """
 
-    print("is_update_file_function: %s" % is_update_file_function)
-    print("update_drive_service_folder_name: %s" % update_drive_service_folder_name)
+
+
+    #print("is_update_file_function: %s" % is_update_file_function)
+    #print("update_drive_service_folder_name: %s" % update_drive_service_folder_name)
 
     store = file.Storage('token.json')
     creds = store.get()
@@ -146,11 +148,11 @@ def main(is_update_file_function=False, update_drive_service_folder_name=None, u
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
-    print('*' * 10)
+    #print('*' * 10)
 
     if is_update_file_function is True:
-        print(update_file_path + update_drive_service_name)
-        print("=====執行上傳檔案=====")
+       # print(update_file_path + update_drive_service_name)
+       # print("=====執行上傳檔案=====")
         # 清空 雲端垃圾桶檔案
         # trashed_file(service=service, is_delete_trashed_file=True)
         get_folder_id = search_folder(service = service, update_drive_folder_name = update_drive_service_folder_name)
@@ -159,18 +161,17 @@ def main(is_update_file_function=False, update_drive_service_folder_name=None, u
                     is_delete_search_file=True)
         # 檔案上傳到雲端上
         update_file(service=service, update_drive_service_name=update_drive_service_name,
-                    local_file_path=os.getcwd() + '/test_data/' + update_drive_service_name, update_drive_service_folder_id=get_folder_id)
-        print("=====上傳檔案完成=====")
+                    local_file_path=os.getcwd() + '/EBM/test_data/' + update_drive_service_name, update_drive_service_folder_id=get_folder_id)
+        #print("=====上傳檔案完成=====")
 
 
 if __name__ == '__main__':
-    dataPath='/home/pi/EBM/'
+    dataPath='/home/pi/EBM/test_data/'
     #dataPath='D:/01.work/00.icebergtek/98.2020_project/E_BM/test_data/'
     allFileList = os.listdir(dataPath)
     allFileList = sorted(allFileList)
     for i in range(len(allFileList)):
-        filepath = allFileList[i]
-        main(is_update_file_function=bool(True), update_drive_service_folder_name='test_upload', update_drive_service_name=allFileList[i], update_file_path=os.getcwd() + '/test_data/')
-        os.remove(dataPath+filepath)
-        print("完成")
-        
+       filepath = allFileList[i]
+       main(is_update_file_function=bool(True), update_drive_service_folder_name='test_upload', update_drive_service_name=allFileList[i], update_file_path=os.getcwd() + '/')
+       os.remove(dataPath+filepath)
+           #print("完成")
